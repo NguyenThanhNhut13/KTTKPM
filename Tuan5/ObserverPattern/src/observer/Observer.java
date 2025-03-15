77c446e3410169689fd89292668c07e5b360b726
NguyenThanhNhut13 <thanhnhutcu@gmail.com>
2025-03-15 11:31:52 +0700

    add Tuan5

---
+++ Tuan5/ObserverPattern/src/observer/Observer.java
@@ -0,0 +1,23 @@
+/*
+ * @ (#) Observer.java       1.0     15/03/2025
+ *
+ * Copyright (c) 2025 IUH. All rights reserved.
+ */
+
+package observer;
+/*
+ * @description:
+ * @author: Nguyen Thanh Nhut
+ * @date: 15/03/2025
+ * @version:    1.0
+ */
+
+public interface Observer {
+
+    //method to update the observer, used by subject
+    public void update();
+
+    //attach with subject to observe
+    public void setSubject(Subject sub);
+
+}
